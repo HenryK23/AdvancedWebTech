73ac996b3c0de046a1947cc8f5ad9fab8b7f4919
HenryK23 <59871978+HenryK23@users.noreply.github.com>
2020-12-01 20:00:03 +0000

    Update masterFlask.py

--- masterFlask.py
+++ masterFlask.py
@@ -1,30 +1,36 @@
 from flask import Flask, render_template, redirect, url_for, request, jsonify, session
 from initQuestionfiles import initBiology, initComputing, initNorwegian, optionList
 app = Flask(__name__)
+#secret key is required for the use of sessions. 
 app.secret_key = 'A0Zr98j /3 yX R~XHH!jmN]LWX / ,? RT'
 
-
+#initialising the pre-made flashcard sets and the option list, this pulls from initQuestionfiles.py 
 initBiology()
 initComputing()
 initNorwegian()
 optionList()
 
+#This will make sure that anyone going to the root page will be redirected to the home page. 
 @app.route('/')
 def base():
     return redirect(url_for("home"))
 
+#to inspect the masterpage without any content 
 @app.route('/flashycards/')
 def root():
     return render_template('MasterPage.html')
 
+#Home page 
 @app.route('/flashycards/home/')
 def home():
     return render_template('homePage.html')
 
+#redundant code, decided to remove the about page. 
 @app.route('/flashycards/home/about/<text>')
 def about(text):
     return render_template('aboutPage.html',text=text)
 
+#Create set page, will ask for the set name and the number of questions so they can be used in the questions page. 
 @app.route('/flashycards/home/createset/', methods=['GET', 'POST'])
 def createset():
     if request.method == 'POST':
@@ -36,22 +42,23 @@ def createset():
     else:
         return render_template('createSet.html')
 
+# Open set page, this will use the option list file to take the option selected in the dropdown and use that to open a flashcard set. 
 @app.route('/flashycards/home/openset/', methods=['GET','POST'])
 def openset():
     if request.method == 'POST':
         session['option'] = request.form['option']
         
         for option in session['optionList']:
-            option = option.replace("\n", "")
+            option = option.replace("\n", "")#blackslash n would get added to the string although it was only being used to create a new line, so it needs removed before the if statement
             if session['option'] == option:
-                fo=open(option+".txt","r")
+                fo=open(option+".txt","r") # all the file naming schemes are the same so this allows for the option to be used to determine the file to open
                 session['questionlist']=fo.readlines()
                 fo.close()
 
-                fr=open(option+"Answers.txt")
+                fr=open(option+"Answers.txt") # again all the file naming conventions are the same so adding Answers to the option selected will always load the answers file. 
                 session['answerlist']=fr.readlines()
                 fr.close()
-                #return(jsonify(session['answerList'], session['questionlist']))
+                #return(jsonify(session['answerList'], session['questionlist'])) # this was to test the returned values 
         return redirect(url_for("flashcard"))
     else:
 
@@ -61,7 +68,7 @@ def openset():
         return render_template('openSet.html', optionList=session['optionList'])
 
 
-
+#this is the questions page where the user sets the questions and answers for their flashcard set. 
 @app.route('/flashycards/home/createset/questions/<int:intQuestionCount><setName>', methods=['GET', 'POST'])
 def questions(intQuestionCount,setName):
 
@@ -72,7 +79,7 @@ def questions(intQuestionCount,setName):
     else:
         return render_template('createQuestion.html', questionCount=intQuestionCount,Name=session['setName'])
 
-
+#this is the flashcard page, it displays the list of questions and once the check answer button is pressed will return check answer page
 @app.route('/flashycards/home/openset/flashcard/', methods=['GET', 'POST'])
 def flashcard():
     if request.method == 'POST':
@@ -82,6 +89,7 @@ def flashcard():
         return render_template('flashCard.html', questionlist=session['questionlist'])
 
 
+#check answer page will calculate the grade and show it on screen
 @app.route('/flashycards/home/openset/flashcard/checkanswer/', methods=['GET','POST'])
 def checkAnswer():
     grade=0
@@ -112,7 +120,7 @@ def checkAnswer():
         return render_template('checkAnswer.html', gradePercentage=gradePercentage, gradeMessage=gradeMessage, letterGrade=letterGrade)
 
 
-
+#this is just a reroute, doesnt open a html file. Will take the current session data and write it into a file which openset page can open. will return to the openset page. 
 @app.route('/flashycards/home/openset/flashcard/checkanswer/save/', methods=['GET','POST'])
 def save():
     if request.method == 'GET':
